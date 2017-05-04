3f077f8abe399735bcd160b5b77bed5931d43e1b
Arnob Karmokar <igeniusarnob@gmail.com>
2017-05-04 15:25:29 +0600

    Add files via upload

---
+++ Lab 1 Assignment/2.cpp
@@ -0,0 +1,13 @@
+/*
+Name: Arnob Karmokar
+ID: 16-01-05-055
+Group: B1
+Year: 2nd
+Semester: 1st
+*/
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<"1. White\n2. Black\n3. Blue \n4. Lime\n5. Green"<<endl;
+  return 0;
+}
---
+++ Lab 1 Assignment/4.cpp
@@ -0,0 +1,15 @@
+/*
+Name: Arnob Karmokar
+ID: 16-01-05-055
+Group: B1
+Year: 2nd
+Semester: 1st
+*/
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<"Patriot "<<"G"<<"a"<<"m"<<"e"<<"\n"; cout
+  << "\t Written"<<" "<<"B"<<"y" << '\n';
+  cout<<"\t\t Tom"<<" "<<"C"<<"lancy\n\n\n";
+  return 0;
+}
