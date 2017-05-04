56fe9d8df53577066ba9f200ee749398589034ab
Arnob Karmokar <igeniusarnob@gmail.com>
2017-05-04 15:24:43 +0600

    Create 1.cpp

---
+++ Lab 1 Assignment/1.cpp
@@ -0,0 +1,13 @@
+/*
+Name: Arnob Karmokar
+ID: 16-01-05-055
+Group: B1
+Year: 2nd
+Semester: 1st
+*/
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<"1. White\t 2. Black\t 3. Blue \t 4. Lime\t 5. Green"<<endl;
+  return 0;
+}
