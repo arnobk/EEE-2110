3270f6b75ddb692c097f64f66ba6c62becea236b
Arnob Karmokar <igeniusarnob@gmail.com>
2017-05-11 22:41:48 +0600

    Create 1.cpp

---
+++ Lab 2 Assignment/1.cpp
@@ -0,0 +1,17 @@
+/*
+Name: Arnob Karmokar
+ID: 16-01-05-055
+Group: B1
+Year: 2nd
+Semester: 1st
+*/
+#include<iostream>
+using namespace std;
+int main(){
+  float a,b;
+  cin>>a>>b;
+  cout<<a<<" + "<<b<<" = "<<a+b<<endl;
+  cout<<a<<" - "<<b<<" = "<<a-b<<endl;
+  cout<<a<<" x "<<b<<" = "<<a*b<<endl;
+  return 0;
+}
