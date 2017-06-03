4c0a51113cfbcc7591306d7d716831572e6efcb2
Arnob Karmokar <igeniusarnob@gmail.com>
2017-06-03 20:50:04 +0600

    Create 1.cpp

---
+++ Lab 4 Assignment/1.cpp
@@ -0,0 +1,20 @@
+/*
+Name: Arnob Karmokar
+ID: 16-01-05-055
+Group: B1
+Year: 2nd
+Semester: 1st
+*/
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cin>>n;
+  for(int i=1;i<=n;i++){
+    for(int j=1;j<=n;j++){
+      cout<<j<<" ";
+    }
+    cout<<"\n";
+  }
+  return 0;
+}
